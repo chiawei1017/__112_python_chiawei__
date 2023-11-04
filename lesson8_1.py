c2ead2cd38e216598233d77f798d49de1e64e8cd
chiawei1017 <147892056+chiawei1017@users.noreply.github.com>
2023-11-04 06:29:03 +0000

    update

--- lesson8_1.py
+++ lesson8_1.py
@@ -6,3 +6,13 @@ print(stu1.chinese)
 print(stu1.english)
 print(stu1.math)
 print(stu1.sum())
+
+
+from tools import getStudent
+
+stu1 = getStudent(name="robert",chinese=89,math=92,english=75)
+print(stu1.name)
+print(stu1.chinese)
+print(stu1.english)
+print(stu1.math)
+print(stu1.sum())
